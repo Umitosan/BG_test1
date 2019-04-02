fe8d5eb96a41122ae297c28b974cb63442b1808a
Umitosan <yeahdom@gmail.com>
2019-04-01 20:44:59 -0700

    vis v3, many styles

--- vis.js
+++ vis.js
@@ -8,16 +8,26 @@ function Visualization() {
   this.spacing = 50;
   this.timeSinceLastMove = undefined;
   this.moveTimer = 4000;
+  this.totalColorPhaseTimer = 5000;
+  this.colorPhaseTimeGap = undefined;
 
   this.init = function() {
-    this.colTotal = Math.floor(winH/this.spacing) - 1;
-    this.rowTotal = Math.floor(winW/this.spacing) - 1;
+    this.colTotal = Math.floor(winW/this.spacing) - 1;
+    this.rowTotal = Math.floor(winH/this.spacing);
     console.log('dimensinos = '+this.colTotal+","+this.rowTotal);
+    this.colorPhaseTimeGap = Math.ceil(this.totalColorPhaseTimer / (this.colTotal * this.rowTotal));
+    let colorPhasePos = 0;
     this.nodes = [];
     for (let cRow = 0; cRow < this.rowTotal; cRow++) {
       let newRow = [];
       for (let cCol = 0; cCol < this.colTotal; cCol++) {
-        let someNode = new Node(((cRow+1)*this.spacing),((cCol+1)*this.spacing),{r:cRow,c:cCol});
+        colorPhasePos += 1;
+        let someNode = new Node(  ((cCol+1)*this.spacing),
+                                  ((cRow+1)*this.spacing),
+                                  {r:cRow,c:cCol},
+                                  // ( this.colorPhaseTimeGap * (cRow+1) * (cCol+1) )
+                                  (this.totalColorPhaseTimer - (this.colorPhaseTimeGap * colorPhasePos))
+                              );
         newRow.push(someNode);
       }
       this.nodes.push(newRow);
@@ -43,7 +53,7 @@ function Visualization() {
     if ((performance.now() - this.timeSinceLastMove) > this.moveTimer) {
       for (let cRow = 0; cRow < this.nodes.length; cRow++) {
         for (let cCol = 0; cCol < this.nodes[cRow].length; cCol++) {
-          this.nodes[cRow][cCol].updateConnection();
+          // this.nodes[cRow][cCol].updateConnection();
         }
       }
       console.log('update happened');
@@ -59,73 +69,149 @@ function Visualization() {
 } // end Visualization()
 
 
-function getRandomNodeCoords(someNode) {
-  let loc = someNode.loc;
-  console.assert(typeof(loc.r) === "number", "nope = "+loc.r);
-  console.assert(typeof(loc.c) === "number", "nope = "+loc.c);
-  console.assert(vis.nodes.length !== undefined, "vis.nodes insn't an array");
-  console.assert(vis.nodes[0].length !== undefined, "vis.nodes[0] insn't an array");
-  if (vis.nodes) {
-    let coordX,coordY;
-    switch (getRandomIntInclusive(1,8)) {
-      case 1: // up
-        if ((loc.r-1) >= 0) {
-          coordX = vis.nodes[loc.r-1][loc.c].x;
-          coordY = vis.nodes[loc.r-1][loc.c].y;
-        } else { coordX = someNode.x; coordY = someNode.y; }
-        break;
-      case 2: // up-right
-        if (((loc.r+1) <= (vis.rowTotal-1)) &&  ((loc.c+1) <= (vis.colTotal-1))) {
-          coordX = vis.nodes[loc.r+1][loc.c+1].x;
-          coordY = vis.nodes[loc.r+1][loc.c+1].y;
-        } else { coordX = someNode.x; coordY = someNode.y; }
-        break;
-      case 3: // right
-        if ((loc.c+1) <= (vis.colTotal-1)) {
-          coordX = vis.nodes[loc.r][loc.c+1].x;
-          coordY = vis.nodes[loc.r][loc.c+1].y;
-        } else { coordX = someNode.x; coordY = someNode.y; }
-        break;
-      case 4: // down-right
-        if ( ((loc.r+1) <= (vis.rowTotal-1)) && ((loc.c+1) <= (vis.colTotal-1)) ) {
-          coordX = vis.nodes[loc.r+1][loc.c+1].x;
-          coordY = vis.nodes[loc.r+1][loc.c+1].y;
-        } else { coordX = someNode.x; coordY = someNode.y; }
-        break;
-      case 5:  // down
-        if ((loc.r+1) <= (vis.rowTotal-1)) {
-          coordX = vis.nodes[loc.r+1][loc.c].x;
-          coordY = vis.nodes[loc.r+1][loc.c].y;
-        } else { coordX = someNode.x; coordY = someNode.y; }
-        break;
-      case 6:  // down-left
-        if ( ((loc.r+1) <= (vis.rowTotal-1)) && ((loc.c-1) >= 0) ) {
-          coordX = vis.nodes[loc.r+1][loc.c-1].x;
-          coordY = vis.nodes[loc.r+1][loc.c-1].y;
-        } else { coordX = someNode.x; coordY = someNode.y; }
-        break;
-      case 7:  // left
-        if ((loc.c-1) >= 0) {
-          coordX = vis.nodes[loc.r][loc.c-1].x;
-          coordY = vis.nodes[loc.r][loc.c-1].y;
-        } else { coordX = someNode.x; coordY = someNode.y; }
-        break;
-      case 8: // up-left
-        if ( ((loc.r-1) >= 0) && ((loc.c-1) >= 0) ) {
-          coordX = vis.nodes[loc.r-1][loc.c-1].x;
-          coordY = vis.nodes[loc.r-1][loc.c-1].y;
-        } else { coordX = someNode.x; coordY = someNode.y; }
-        break;
-      default:
-        console.log('switch probs');
-        break;
-    }
-    return { x:coordX, y:coordY };
+function getRandomDir(goodNums, badDirs = undefined) {
+  let newGood;
+  if (badDirs !== undefined) {
+    newGood = goodNums.filter( function(num) {
+      if ( badDirs.includes(num) === false ) {
+        return num;
+      }
+    });
+    // console.assert(typeof(newGood) === 'object', 'not and object');
+    // console.log('badDirs = ', badDirs);
+    // console.log('goodNums,newGood = '+goodNums+"  --  "+newGood);
+    goodNums = newGood;
   }
+  let randNum = getRandomIntInclusive(1,8);
+  while (goodNums.includes(randNum) !== true) {
+    randNum = getRandomIntInclusive(1,8);
+  }
+  return randNum;
 }
 
 
-function Node(x,y,indices) {
+function getRandomNodeCoords(someNode) {
+  let loc = someNode.loc;
+  let randDir = 0;
+  let badDirs;
+
+  if (someNode.connectedCoords1 !== undefined) {
+    let anotherBadDir;
+    switch (someNode.connectedCoords1.dir) {
+      case 1:
+        anotherBadDir = 5; break;
+      case 2:
+        anotherBadDir = 6; break;
+      case 3:
+        anotherBadDir = 7; break;
+      case 4:
+        anotherBadDir = 8; break;
+      case 5:
+        anotherBadDir = 1; break;
+      case 6:
+        anotherBadDir = 2; break;
+      case 7:
+        anotherBadDir = 3; break;
+      case 8:
+        anotherBadDir = 4; break;
+      default:
+        console.log('bad switch!');
+        break;
+    }
+    badDirs = [someNode.connectedCoords1.dir,anotherBadDir];
+  }
+
+  if ( (loc.r === 0) && (loc.c === 0) ) { // top-left corner
+    randDir = getRandomDir([3,4,5],badDirs);
+  } else if ( (loc.r === 0) && (loc.c === (vis.colTotal-1)) ) { // top-right corner
+    randDir = getRandomDir([5,6,7],badDirs);
+  } else if ( (loc.r === (vis.rowTotal-1)) && (loc.c === (vis.colTotal-1)) ) { // bot-right corner
+    randDir = getRandomDir([1,7,8],badDirs);
+  } else if ( (loc.r === (vis.rowTotal-1)) && (loc.c === 0) ) { // bot-left corner
+    randDir = getRandomDir([1,2,3],badDirs);
+  } else if (loc.r === 0) { // top
+    randDir = getRandomDir([3,4,5,6,7],badDirs);
+  } else if (loc.c === (vis.colTotal-1)) { // right
+    randDir = getRandomDir([1,5,6,7,8],badDirs);
+  } else if (loc.r === (vis.rowTotal-1)) { // bot
+    randDir = getRandomDir([1,2,3,7,8],badDirs);
+  } else if (loc.c === 0) { // left
+    randDir = getRandomDir([1,2,3,4,5],badDirs);
+  } else {
+    randDir = getRandomDir([1,2,3,4,5,6,7,8],badDirs);
+  }
+
+  console.assert(randDir !== 0,'randDir probs');
+
+  let coordX,coordY;
+  let coordRow, coordCol;
+  let dirNum;
+  switch (randDir) {
+    case 1: // up
+        coordX = vis.nodes[loc.r-1][loc.c].x;
+        coordY = vis.nodes[loc.r-1][loc.c].y;
+        coordRow = loc.r-1;
+        coordCol = loc.c;
+        dirNum = 1;
+      break;
+    case 2: // up-right
+        coordX = vis.nodes[loc.r-1][loc.c+1].x;
+        coordY = vis.nodes[loc.r-1][loc.c+1].y;
+        coordRow = loc.r-1;
+        coordCol = loc.c+1;
+        dirNum = 2;
+      break;
+    case 3: // right
+        coordX = vis.nodes[loc.r][loc.c+1].x;
+        coordY = vis.nodes[loc.r][loc.c+1].y;
+        coordRow = loc.r;
+        coordCol = loc.c+1;
+        dirNum = 3;
+      break;
+    case 4: // down-right
+        coordX = vis.nodes[loc.r+1][loc.c+1].x;
+        coordY = vis.nodes[loc.r+1][loc.c+1].y;
+        coordRow = loc.r+1;
+        coordCol = loc.c+1;
+        dirNum = 4;
+      break;
+    case 5:  // down
+        coordX = vis.nodes[loc.r+1][loc.c].x;
+        coordY = vis.nodes[loc.r+1][loc.c].y;
+        coordRow = loc.r+1;
+        coordCol = loc.c;
+        dirNum = 5;
+      break;
+    case 6:  // down-left
+        coordX = vis.nodes[loc.r+1][loc.c-1].x;
+        coordY = vis.nodes[loc.r+1][loc.c-1].y;
+        coordRow = loc.r+1;
+        coordCol = loc.c-1;
+        dirNum = 6;
+      break;
+    case 7:  // left
+        coordX = vis.nodes[loc.r][loc.c-1].x;
+        coordY = vis.nodes[loc.r][loc.c-1].y;
+        coordRow = loc.r+1;
+        coordCol = loc.c-1;
+        dirNum = 7;
+      break;
+    case 8: // up-left
+        coordX = vis.nodes[loc.r-1][loc.c-1].x;
+        coordY = vis.nodes[loc.r-1][loc.c-1].y;
+        coordRow = loc.r-1;
+        coordCol = loc.c-1;
+        dirNum = 8;
+      break;
+    default:
+      console.log('switch probs');
+      break;
+  }
+  return { x:coordX, y:coordY, r:coordRow , c:coordCol, dir:dirNum };
+}
+
+
+function Node(x,y,indices,colorOffset) {
   this.x = x;
   this.y = y;
   this.baseX = x;
@@ -133,35 +219,53 @@ function Node(x,y,indices) {
   this.solidColor = randColor('rgba');
   this.lineColor = randColor('rgba');
   this.loc = indices;
-  this.connectedCoords = undefined;
-  this.xVel = (getRandomIntInclusive(1,2)/20) * randSign();
-  this.yVel = (getRandomIntInclusive(1,2)/20) * randSign();
+  this.connectedCoords1 = undefined;
+  this.connectedCoords2 = undefined;
+  this.xVel = (getRandomIntInclusive(1,2)/10) * randSign();
+  this.yVel = (getRandomIntInclusive(1,2)/10) * randSign();
+  this.lastColorChangeTime = undefined;
+  this.colorTimerOffset = colorOffset;
 
   this.init = function() {
-    this.connectedCoords = getRandomNodeCoords(this);
+    this.connectedCoords1 = getRandomNodeCoords(this);
+    this.connectedCoords2 = getRandomNodeCoords(this);
+    this.lastColorChangeTime = performance.now() - this.colorTimerOffset;
+    // console.log('node init complete: ', this.loc);
   };
 
   this.draw = function() {
-    ctx.save();
+
+    // ctx.save();
+    // ctx.beginPath();
+    // ctx.translate(this.x,this.y);
+    // ctx.fillStyle = this.lineColor;
+    // ctx.strokeStyle = this.lineColor;
+    // ctx.fillRect(-3,-3,6,6);  // void ctx.fillRect(x, y, width, height);
+    // ctx.restore();
+
     ctx.beginPath();
-    ctx.translate(this.x,this.y);
+    // ctx.strokeStyle = this.lineColor;
     ctx.fillStyle = this.lineColor;
-    ctx.strokeStyle = this.lineColor;
-    ctx.fillRect(-3,-3,6,6);  // void ctx.fillRect(x, y, width, height);
-    ctx.restore();
-    ctx.beginPath();
-    ctx.strokeStyle = this.lineColor;
-    ctx.lineWidth = 2;
+    // ctx.lineWidth = 2;
     ctx.moveTo(this.x,this.y);
-    ctx.lineTo(this.connectedCoords.x,this.connectedCoords.y);
-    ctx.stroke();
+    ctx.lineTo(this.connectedCoords1.x,this.connectedCoords1.y);
+    ctx.lineTo(this.connectedCoords2.x,this.connectedCoords2.y);
+    ctx.lineTo(this.x,this.y);
+    // ctx.stroke();
+    ctx.fill();
+
   };
 
   this.updateConnection = function() {
-    this.connectedCoords = getRandomNodeCoords(this);
+    this.connectedCoords1 = getRandomNodeCoords(this);
+    this.connectedCoords2 = getRandomNodeCoords(this);
   };
 
   this.update = function() {
+    if ((performance.now() - this.lastColorChangeTime) > vis.totalColorPhaseTimer) {
+      this.lineColor = randColor("rgba");
+      this.lastColorChangeTime = performance.now();
+    }
     if ( Math.abs((this.x + this.xVel) - this.baseX) > vis.spacing ) {
       this.xVel *= -1;
     }
